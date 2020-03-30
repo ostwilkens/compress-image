ffe50f90a2dde57df4e7036d7dbe826830819859
Carl Öst Wilkens <carl@ostwilkens.se>
2020-03-31 00:06:55 +0200

    Refactor

--- index.js
+++ index.js
@@ -1,21 +1,14 @@
 function process(el) {
   const files = [...el.files];
-  (new Compress()).compress(files).then((conversions) => {
+  new Compress().compress(files).then((conversions) => {
     const { photo } = conversions[0];
     const url = URL.createObjectURL(photo.data);
-
-    var saveData = (function () {
-      var a = document.createElement("a");
-      document.body.appendChild(a);
-      a.style = "display: none";
-      return function (url, filename) {
-        a.href = url;
-        a.download = filename;
-        a.click();
-        window.URL.revokeObjectURL(url);
-      };
-    }());
-
-    saveData(url, photo.name);
+    var a = document.createElement("a");
+    document.body.appendChild(a);
+    a.style = "display: none";
+    a.href = url;
+    a.download = photo.name;
+    a.click();
+    window.URL.revokeObjectURL(url);
   })
 }
